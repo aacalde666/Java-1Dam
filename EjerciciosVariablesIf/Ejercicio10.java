deb8f208f29a44632e8aacc52e2aa4f19972a287
aacalde666 <aacalde666@gmail.com>
2023-10-11 22:39:17 +0200

    Ejercicios de clase

--- EjerciciosVariablesIf/Ejercicio10.java
+++ EjerciciosVariablesIf/Ejercicio10.java
@@ -19,12 +19,17 @@ public class Ejercicio10 {
 		double long2 = teclado.nextDouble();
 		System.out.print("Escribe el tercer lado: ");
 		double long3 = teclado.nextDouble();
-		if (long1==long2&&long2==long3) {
-			System.out.println("Es un triangulo Equilatero");
-		}else if (long1==long2||long2==long3||long3==long1) {
-			System.out.println("Es un triangulo Isosceles");
+		boolean tri = (long1 + long2 > long3) || (long1 + long3 > long2) || (long2 + long3 > long1);
+		if (tri) {
+			if (long1==long2&&long2==long3) {
+				System.out.println("Es un triangulo Equilatero");
+			}else if (long1==long2||long2==long3||long3==long1) {
+				System.out.println("Es un triangulo Isosceles");
+			}else {
+				System.out.println("Es un triangulo Escaleno");
+			}
 		}else {
-			System.out.println("Es un triangulo Escaleno");
+			System.out.println("No es un triangulo");
 		}
 	}
 
