4bfd20f18e2ace02d37db20c7a0dd5b6c2f4bcee
aacalde666 <aacalde666@gmail.com>
2023-10-15 11:31:07 +0200

    Add files via upload

---
+++ EjerciciosVariables2/Ejercicio5.java
@@ -0,0 +1,29 @@
+package EjerciciosVariables2;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		//Ejercicio5
+		/*Recibir un número entre 1 y 99 e indicar si su decena es un número par o impar*/
+		@SuppressWarnings("resource")
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Escribe un número entre 1 y 99 para saber si su decena es par o impar");
+		int n = teclado.nextInt();
+		//int uni;
+		int dec;
+		if (n>1 && n<99) {
+			//uni = n/10;
+			dec = n%10;
+			if (dec%2==0) {
+				System.out.println("La decena del número "+n+" es par: "+dec);
+			}else {
+				System.out.println("La decena del número "+n+" es impar: "+dec);
+			}
+		}else {
+			System.out.println("Sobrepasastes de los números que te hemos indicado");
+		}
+	}
+
+}
