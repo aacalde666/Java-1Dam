cfadc1ecb49a182dbf7cda0a1ddb3df377c33ca4
aacalde666 <aacalde666@gmail.com>
2023-11-03 14:19:55 +0100

    Ejemplos de clase

--- EjemplosEnClase/EjemploNumerosAleatorios.java
+++ EjemplosEnClase/EjemploNumerosAleatorios.java
@@ -5,8 +5,14 @@ public class EjemploNumerosAleatorios {
 	public static void main(String[] args) {
 		//Math.random() funciona en double
 		//Generar número aleatorio
-		for (int i=0;i<10;i++) {
+		/*for (int i=0;i<10;i++) {
 			int n=(int)(Math.random()*101);
+			System.out.println(n);
+		}*/
+		//generar un número aleatorio entre a y b
+		int a=12, b=20;
+		for(int i=0;i<10;i++) {
+			int n=(int)(Math.random()*(b-a))+a;
 			System.out.println(n);
 		}
 	}
